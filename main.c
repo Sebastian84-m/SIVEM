87c3b71f42f2eb2658ce67eb33c28c80d24d5581
023daniii <agb.dan.zzz@gmail.com>
2025-06-17 01:03:30 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "menu.h"
-#include "registro.h"
-#include "matricula.h"
-#include "revision.h"
+#include <stdio.h>    // Incluye la librería estándar de entrada/salida (para printf/scanf)
+#include "menu.h"       
+#include "registro.h"    
+#include "matricula.h"   
+#include "revision.h"    
+
+//declaramos las funciones
 
 int main() {
 	int opcion;
 	
+	//creamos el bucle de la funcion mostrarMenu
 	do {
 		mostrarMenu();
 		scanf("%d", &opcion);
-		limpiarBuffer();
-		procesarOpcion(opcion);
+		procesarOpcion(opcion); //Ejecuta una acción según la opción seleccionada por el usuario.
 		
 	} while(opcion != 5);
-
-
+	
+	
 	return 0;
 }
