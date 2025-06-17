d712eac537d3fb17a0d87345349fc6d7a25adbbc
023daniii <agb.dan.zzz@gmail.com>
2025-06-17 01:02:14 -0500

    Update matricula.c

--- matricula.c
+++ matricula.c
@@ -1,18 +1,27 @@
-a#include <stdio.h>
+#include <stdio.h>
 #include "registro.h"
 #include "matricula.h"
 
-extern struct Vehiculo lista[];
-extern int total;
+//Usa variables globales definidas externamente.
+
+extern struct Vehiculo lista[];   //arreglo global de vehículos.
+extern int total;                 //cuántos vehículos hay registrados
+
 void calcularMatricula() {
 	if (total == 0) {
 		printf("\nNo hay vehiculos registrados.\n");
-		return;
+		return; // Sale de la función si la lista está vacía
 	}
 	
-	printf("\n--- Vehiculos Registrados ---\n");
-	for (int i = 0; i < total; i++) {
-		printf("=============\n\nVehiculo:%d\nPlaca:%s\nTipo:%s\nRevisiones Tecnicas:%d\nModelo:%s\nAvaluo:$%.2f\nCosto matricula:$%.2f\n  ", 
-			   i+1,lista[i].placa,lista[i].tipo, lista[i].revisiones,lista[i].modelo,lista[i].avaluo, lista[i].costo);
+	printf("\n===== Vehiculos Registrados =====\n");
+	for (int i = 0; i < total; i++) {  //Recorre todos los vehículos registrados para procesarlos uno a uno.
+		printf("=============\n");
+		printf("Vehiculo: %d\n", i + 1);
+		printf("Placa: %s\n", lista[i].placa);
+		printf("Tipo: %s\n", lista[i].tipo);
+		printf("Revisiones Tecnicas: %d\n", lista[i].revisiones);
+		printf("Modelo: %s\n", lista[i].modelo);
+		printf("Avaluo: $%.2f\n", lista[i].avaluo);
+		printf("Costo matricula: $%.2f\n\n", lista[i].costo);
 	}
 }
